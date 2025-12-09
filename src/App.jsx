732e2f1fb5aa22dcb35dc00535b0084f904ad768
Ilya Hvostenko <ilyajavadev@gmail.com>
2025-12-09 11:14:50 +0200

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+    return (
+       <main>
+           <h1 className='text-center text-5xl text-orange-500 font-bold'>Welcome to Awwwards</h1>
+       </main>
+    )
+}
+export default App
